ab3229276e50b42e93104935efb5af0acc30ab07
Victor Dasilva <victor.dasilva@incluit.com>
2021-10-29 11:12:52 -0300

    Deploy

---
+++ dist/precache-manifest.af784829b652cb755f7c3358b9f64f33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b97e04d906460777a5b6",
+    "url": "/css/app.c981b2ff.css"
+  },
+  {
+    "revision": "ab0a54a39da10e8e333549ba7af803e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b97e04d906460777a5b6",
+    "url": "/js/app.25e482fb.js"
+  },
+  {
+    "revision": "4fdaa2d8a8be8939afe1",
+    "url": "/js/chunk-vendors.4d7f513e.js"
+  },
+  {
+    "revision": "8f049ebb353eb18c9ab644651d29fc04",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
